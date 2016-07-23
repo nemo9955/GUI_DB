1e96c0d751f4c391a2ea9bdfa18c4b728eb57a4e
nemo9955 <nemo9955@gmail.com>
2016-07-23 10:24:54 +0300

    initial push

---
+++ src/nemo9955/Main.java
@@ -0,0 +1,9 @@
+package nemo9955;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/nemo9955/MainGUI.java
@@ -0,0 +1,12 @@
+package nemo9955;
+
+import javax.swing.JPanel;
+
+public class MainGUI  extends JPanel {
+	private static final long serialVersionUID = 8060823913806665474L;
+	
+	public MainGUI() {
+		
+	}
+
+}
