c9379105a70671e24c56e306b7f4774fdcbfa7d6
nemo9955 <nemo9955@gmail.com>
2016-07-24 16:02:46 +0300

    created startDBMaster42.jar executable for convenience

--- src/nemo9955/DBUtil.java
+++ src/nemo9955/DBUtil.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
@@ -14,9 +13,6 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-
-import org.apache.derby.iapi.services.io.ArrayUtil;
 
 import nemo9955.Act.ComandResult;
 
